a87de4f741a6f797e4aabe41407ea5971a72caea
Ronny2101 <dilshoddaminov2121@gmail.com>
2025-05-24 18:22:24 +0900

    feat: create useGlobals integrated with Context & develop logout process

---
+++ src/app/context/ContextProvider.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode, useState } from "react";
+import { Member } from "../../lib/types/member";
+import { GlobalContext } from "../hooks/useGlobals";
+import Cookies from "universal-cookie";
+
+const ContextProvider: React.FC<{children: ReactNode }> = ({children}) => {
+    const cookies = new Cookies();
+    if (!cookies.get("accesToken")) localStorage.removeItem("memberData");
+
+    const [authMember, setAuthMember] = useState< Member | null> (
+        localStorage.getItem("memberData") 
+        ? JSON.parse(localStorage.getItem("memberData") as string)
+        : null
+    );
+    console.log("=== verify ===");
+
+    return   (
+        <GlobalContext.Provider value={{ authMember, setAuthMember }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+};
+
+export default ContextProvider;
