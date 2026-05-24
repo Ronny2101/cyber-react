faf2b0ad57e3efc9a965bb25ef47c490407cfe23
Ronny2101 <dilshoddaminov2121@gmail.com>
2026-05-24 23:22:06 +0900

    feat: deploy project via shell & pm2

---
+++ process.config.js
@@ -0,0 +1,20 @@
+// const { name } = require("ejs");
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "CYBER",
+//       cwd: "./",
+//       script: "./dist/server.js",
+//       watch: false,
+//       env_produxtion: {
+//         NODE_ENV: "production",
+//       },
+//       env_development: {
+//         NODE_ENV: "development",
+//       },
+//       instances: 1,
+//       exec_mode: "cluster",
+//     },
+//   ],
+// };
